745b163843ff654e2e1af8c1be35a75a79241d6c
yassin-aberkan <yaberkan.pro@gmail.com>
2024-11-03 21:19:44 +0100

    fix environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/api',
+  baseUrl: 'https://brusssels-shuttle.be:8080/api',
   STRIPE_PUBLIC_KEY: 'pk_test_51QCiu7GCKWP9kq3ldkIe3rB2DadmLjamtTancvpih6sdHFlVK58bPzBmFXqxIUUEJCInurg6rCRqy2UbKUsVJhOi003CQSfy7e'
 };
