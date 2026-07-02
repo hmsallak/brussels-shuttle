b14fca7c595b97173c2773c46a3933bbbe3c6133
msallak <hilyas707@gmail.com>
2026-07-02 23:48:24 +0200

    Develop homepage service funnels

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,13 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {ReservationApproachComponent} from "./reservation-approach/reservation-approach.component";
-import {TitleComponent} from "../../shared/components/title/title.component";
 import {LayoutComponent} from "../../shared/components/layout/layout.component";
 import {RouterLink} from "@angular/router";
-import {FeaturesComponent} from "./features/features.component";
 import {fadeInUpAnimation} from "angular-animations";
 import {TestimonialComponent} from "./testimonial/testimonial.component";
 import {DestinationComponent} from "../services/destination-page/destination/destination.component";
 import {BookingFormComponent} from "../../shared/components/booking-from/booking-form.component";
 import {TuiInputDateModule} from "@taiga-ui/kit";
 import {TuiTextfieldControllerModule} from "@taiga-ui/core";
-import {LowerCasePipe} from "@angular/common";
 import {scrollToSection} from "../../shared/utils/element.utils";
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {toSignal} from "@angular/core/rxjs-interop";
@@ -21,17 +18,14 @@ import {startWith, switchMap} from "rxjs";
   standalone: true,
   imports: [
     ReservationApproachComponent,
-    TitleComponent,
     BookingFormComponent,
     LayoutComponent,
     RouterLink,
-    FeaturesComponent,
     TestimonialComponent,
     DestinationComponent,
     TuiInputDateModule,
     TuiTextfieldControllerModule,
-    TranslateModule,
-    LowerCasePipe
+    TranslateModule
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
@@ -42,6 +36,62 @@ import {startWith, switchMap} from "rxjs";
 })
 export class HomePageComponent {
   private translateService = inject(TranslateService);
+
+  serviceCards = [
+    {
+      kicker: 'Aeroports',
+      title: 'Transferts vers tous les aeroports',
+      description: 'Zaventem, Charleroi, CDG, Orly, Schiphol, Eindhoven, Luxembourg, Dusseldorf et plus.',
+      image: 'assets/images/zaventem.webp'
+    },
+    {
+      kicker: 'Evenements',
+      title: 'Festivals, concerts et sorties',
+      description: 'Tomorrowland, Les Ardentes, festivals a Bruxelles, retours tardifs et trajets de groupe.',
+      image: 'assets/images/schuman.webp'
+    },
+    {
+      kicker: 'Belgique',
+      title: 'Villes belges et tourisme',
+      description: 'Brugge, Gand, Liege, Namur, Anvers, Mons, Dinant ou Spa avec chauffeur prive.',
+      image: 'assets/images/brussels-shuttle-bg-phone.webp'
+    }
+  ];
+
+  eventRoutes = [
+    'Tomorrowland - Boom',
+    'Les Ardentes - Liege',
+    'Festival Bruxelles',
+    'Concerts et nightlife',
+    'Salons business',
+    'Mariages et groupes'
+  ];
+
+  quickRoutes = [
+    'Bruxelles -> Zaventem',
+    'Bruxelles -> Charleroi',
+    'Bruxelles -> Brugge',
+    'Bruxelles -> Gent',
+    'Bruxelles -> Liege',
+    'Bruxelles -> Namur',
+    'Bruxelles -> CDG',
+    'Bruxelles -> Schiphol'
+  ];
+
+  blogTeasers = [
+    {
+      title: 'Aller a Tomorrowland avec chauffeur prive',
+      description: 'Comment organiser depart, retour tardif et trajet de groupe vers Boom.'
+    },
+    {
+      title: 'Brugge, Gand, Liege, Namur en une journee',
+      description: 'Des idees de voyages en Belgique sans dependance aux horaires de train.'
+    },
+    {
+      title: 'Quel aeroport choisir depuis Bruxelles ?',
+      description: 'Zaventem, Charleroi, CDG, Orly, Schiphol: les criteres utiles.'
+    }
+  ];
 
   destinationsItems = toSignal(
     this.translateService.onLangChange.pipe(
